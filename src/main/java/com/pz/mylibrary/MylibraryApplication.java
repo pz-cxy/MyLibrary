8286acbadfa348ba892a2a75e032ed2ebdf265a3
pz-cxy <m17324426187@163.com>
2020-05-26 22:50:21 +0800

    图书馆预约管理系统

---
+++ src/main/java/com/pz/mylibrary/MylibraryApplication.java
@@ -0,0 +1,14 @@
+package com.pz.mylibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MylibraryApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MylibraryApplication.class, args);
+    }
+
+}
